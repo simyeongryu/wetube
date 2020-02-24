cefb1eb6ee0da2a1246f9dc7bb75abb07d955ae2
simyeongryu <rsm0503@gmail.com>
2020-02-24 23:32:25 +0900

    시명 유저 라우터 접근 순서 변경

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,9 +7,12 @@ export const logout = (req, res) =>
     res.render("logout", {pageTitle: "LogOut"});
 export const users = (req, res) =>
     res.render("users", {pageTitle: "Users"});
-export const userDetail = (req, res) =>
-    res.render("userDetail", {pageTitle: "UserDetail"});
 export const editProfile = (req, res) =>
     res.render("editProfile", {pageTitle: "EditProfile"});
 export const changePassword = (req, res) =>
     res.render("changePassword", {pageTitle: "ChangePassword"});
+export const userDetail = (req, res) =>
+    res.render("userDetail", {pageTitle: "UserDetail"});
+
+
+
